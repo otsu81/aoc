ab637d92f9f61d8f98374c4ac7025f109317a1bf
o <carl@otsu.se>
2024-12-14 23:46:05 +0100

    day2 part 2

--- aoc2024/02/main.go
+++ aoc2024/02/main.go
@@ -27,33 +27,59 @@ func splitToArrays(path string) [][]int {
 
 func checkSafe(i1, i2, posOrNeg int) bool {
 	v := (i1 - i2) * posOrNeg
-	safe := 1 <= v && v <= 3
-	return safe
+	if v == 0 {
+		return false
+	}
+	return 1 <= v && v <= 3
 }
 
-func checkLevel(level []int) (bool, int) {
+func checkLevel(level []int) bool {
 	safe := true
 	var posOrNeg int
-	unsafeIndex := -1
 	if level[0] > level[1] {
 		posOrNeg = 1
+	} else if level[0]-level[1] == 0 {
+		return false
 	} else {
 		posOrNeg = -1
 	}
+
 	for i := 1; i < len(level); i++ {
 		if !checkSafe(level[i-1], level[i], posOrNeg) {
 			safe = false
-			unsafeIndex = i - 1
 			break
 		}
 	}
-	return safe, unsafeIndex
+	return safe
+}
+
+func problem2(a [][]int) {
+	safesum := 0
+	for _, level := range a {
+		safe := checkLevel(level)
+		if safe {
+			safesum++
+			continue
+		}
+
+		for i := range level {
+			sublevel := append([]int{}, level[:i]...)
+			sublevel = append(sublevel, level[i+1:]...)
+			safe = checkLevel(sublevel)
+			if safe {
+				safesum++
+				break
+			}
+		}
+
+	}
+	fmt.Println("problem 2:", safesum)
 }
 
 func problem1(a [][]int) {
 	safeSum := 0
 	for _, level := range a {
-		safe, _ := checkLevel(level)
+		safe := checkLevel(level)
 		if safe {
 			safeSum++
 		}
@@ -65,5 +91,5 @@ func problem1(a [][]int) {
 func main() {
 	a := splitToArrays("./input.txt")
 	problem1(a)
-	// problem2(a)
+	problem2(a)
 }
